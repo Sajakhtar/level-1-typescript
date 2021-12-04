69ee838191fc987299fff32c327cbef7e541111b
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-12-04 17:34:24 +0400

    add basic types

--- dist/index.46269f4e.js
+++ dist/index.46269f4e.js
@@ -1,3 +1,36 @@
-console.log('hello world');
+// console.log('hello world');
+// TYPES
+const isOpen = false;
+const firstName = "Jon";
+const age = 30.5;
+const list = [
+    0,
+    1,
+    2,
+    3
+];
+const person = [
+    'Jon',
+    30.5
+]; // tuple
+const person2 = [
+    'Jon',
+    30.5,
+    false
+]; // tuple
+var Job1// enum (like a list)
+;
+(function(Job) {
+    Job[Job["WebDev"] = 0] = "WebDev";
+    Job[Job["WebDesigner"] = 1] = "WebDesigner";
+    Job[Job["PM"] = 2] = "PM";
+})(Job1 || (Job1 = {
+}));
+const job = Job1.WebDesigner;
+const phone = "iPhone 12 mini";
+const laptop = 2;
+// Never should be useful as a result of a function
+const tablet = 3; // doesnt make sense in this context
+ // Type of 'object', when the other types are not applicable
 
 //# sourceMappingURL=index.46269f4e.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,29 @@
-console.log('hello world');
+// console.log('hello world');
+
+
+// TYPES
+
+const isOpen: boolean = false;
+
+const firstName: string = "Jon";
+
+const age: number = 30.5;
+
+const list: number[] = [0,1,2,3];
+
+const person: [string, number] = ['Jon', 30.5];  // tuple
+
+const person2: [string, number, boolean] = ['Jon', 30.5, false];  // tuple
+
+enum Job { WebDev, WebDesigner, PM }  // enum (like a list)
+const job: Job = Job.WebDesigner;
+
+
+const phone: any = "iPhone 12 mini";
+const laptop: any = 2;
+
+// Never should be useful as a result of a function
+const tablet: never = 3; // doesnt make sense in this context
+
+
+// Type of 'object', when the other types are not applicable
